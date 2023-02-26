bc7d08d1ae6f0657b3f4ea08300b318952c828ad
Emre BALCI <116108168+e1982b@users.noreply.github.com>
2023-02-26 21:02:00 +0100

    Update NewClass_1.java

--- src/NewClass_1.java
+++ src/NewClass_1.java
@@ -1,5 +1,7 @@
 public class NewClass_1 {
     public static void main(String[] args) {
         System.out.println("I am learning GitHub!");
+        System.out.println("This is added from GitHub!");
+
     }
 }
